7e061af6b712419118dbaad76988327f54534bbe
benjaminfra <francheteau.benjamin@gmail.com>
2023-07-24 16:17:58 +0200

    delete pronostic on delete game

--- server/db/models/gameModel.ts
+++ server/db/models/gameModel.ts
@@ -1,5 +1,6 @@
 import { Schema, Types, model } from 'mongoose'
 import { ITeam } from './teamModel'
+import { Pronostic } from './pronosticModel'
 
 export type IGame = {
   _id: Types.ObjectId
@@ -34,5 +35,16 @@ const GameSchema = new Schema<IGame>({
 })
 
 GameSchema.index({ homeTeam: 1, awayTeam: 1, weekNumber: 1 }, { unique: true })
+
+GameSchema.pre<IGame>('deleteOne', async function deletePronostics() {
+  const gameId = this._id
+  try {
+    await Pronostic.deleteMany({ gameId }).exec()
+  } catch (error) {
+    console.log(
+      `Impossible de supprimer les pronostics liés au match ${gameId}`
+    )
+  }
+})
 
 export const Game = model<IGame>('games', GameSchema)
